19f8c4ede3a7550fd2f3fb9dfdd0aa92d60e6299
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-08 16:17:32 +0300

    updated: delegates

--- app/src/main/java/com/ismailmesutmujde/kotlindelegates/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlindelegates/MainActivity.kt
@@ -2,12 +2,49 @@ package com.ismailmesutmujde.kotlindelegates
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LifecycleLogger by LifecycleLoggerImplementation() {
+
+    // property delegates
+    private val myVariable by lazy {
+        println("hello this is a lazy implementation")
+        10
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        registerLifecycleOwner(this)
+
+        println(myVariable)
+    }
+}
+
+interface LifecycleLogger{
+    fun registerLifecycleOwner(owner: LifecycleOwner)
+
+}
+
+class LifecycleLoggerImplementation : LifecycleLogger, LifecycleEventObserver {
+    override fun registerLifecycleOwner(owner: LifecycleOwner) {
+        owner.lifecycle.addObserver(this)
+    }
+
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        when(event) {
+            Lifecycle.Event.ON_CREATE -> println("on create executed")
+            Lifecycle.Event.ON_RESUME -> println("on resume executed")
+            Lifecycle.Event.ON_PAUSE -> println("on pause executed")
+            Lifecycle.Event.ON_START -> println("on start executed")
+            Lifecycle.Event.ON_STOP -> println("on stop executed")
+            Lifecycle.Event.ON_DESTROY -> println("on destroy executed")
+            Lifecycle.Event.ON_ANY -> println("on any executed")
+            else -> Unit
+
+        }
     }
 }
